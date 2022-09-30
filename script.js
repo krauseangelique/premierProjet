5d222407854e40ada2aa1a8a4e2014c3329510a2
angelique KRAUSE <krause_angelique@hotmail.com>
2022-09-30 15:59:44 +0200

    variables and console.log

---
+++ script.js
@@ -0,0 +1,16 @@
+const firstName = "Lucas";
+const age = 29;
+const city = "Waremme";
+/*
+let array = Array[];
+let object = Object{};
+*/
+let isConnected; 
+isConnected =true;
+
+
+
+console.log("Bonjour le monde");
+console.log(`j'aimerai le savoir`);
+console.log("je m'appelle " + firstName + ", j'ai " + age + " ans et j'habite " + city);
+
